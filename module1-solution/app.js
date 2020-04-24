154e47fd5d917a7b3f86ca380e68af0a11ad1b66
Gino <neodracarys@gmail.com>
2020-04-25 00:53:11 +0800

    Module 1 Coding Assignment Implementation

---
+++ module1-solution/app.js
@@ -0,0 +1,48 @@
+(function () {
+    'use strict';
+    angular.module('lunchCheck', [])
+        .controller('LunchCheckController', LunchCheckController)
+
+    LunchCheckController.$inject = ['$scope', '$filter'];
+    const maxLimit= 3;
+
+    function LunchCheckController($scope, $filter) {
+        $scope.message="";
+        $scope.userInput="";
+
+        $scope.processUserInput= () => {
+            //console.log("MCI> processUserInput");
+            if(checkIfEmpty($scope.userInput)){
+                $scope.message="Please enter data first";
+            } else if (checkIfTooMuch($scope.userInput)){
+                $scope.message="Too much!";
+            } else {
+                $scope.message="Enjoy!";
+            }
+        }
+    }
+
+    // Check if which items are not empty.
+    // Return true if nonempty items exceed the limit
+    function checkIfTooMuch(input){
+        //console.log("MCI> checkIfTooMuch");
+        let inputArray= input.split(",");
+        let count= 0;
+
+        inputArray.forEach((item)=>{
+            if(item.length > 0){
+                count++;
+            }
+        })
+
+        //console.log("MCO> checkIfTooMuch");
+        return count > maxLimit;
+    }
+
+    function checkIfEmpty(input) {
+        //console.log("MCI> checkIfEmpty");
+        return input.length < 1;
+    }
+
+
+})();
